619b951a047dd80027fc1f1967fc570dbc67078a
lucas <lucas@pop-os.localdomain>
2024-01-16 22:11:18 -0300

    second

--- consulta.py
+++ consulta.py
@@ -6,3 +6,17 @@ mysql = BDMySQL('tributo')
 consulta = pd.read_excel(os.getenv('ARQUIVO'))
 
 print(consulta)
+
+# extrair cnpj utilizando pandas  
+
+# transformar cnpj em cnpjbase
+
+# remover cnpj base repetidos 
+
+# consultar tabela no banco buscando cnpj ordenado pelo ano [0]
+# se não encontrar busca cnpj base ordenado pelo ano [0]
+# cria uma lista com as consultas se ja cnpj base ja estiver sido consultado, não consulta e pega a forma de tributação
+
+
+# gravar na tabela as informações extraidas do banco (forma tributação e ano)
+
